6b3c3a07e3e89edb23289defaa120bc8e731e52d
dayronCastilloH <thecodedevcompany0@gmail.com>
2024-07-27 01:35:56 -0600

    adding BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css';
 import { MainApp } from './useContext/MainApp';
+import { BrowserRouter } from 'react-router-dom';
 
 // import { Padre } from './usando-memo/Padre';
 // import { PokeApp } from './pokemon-app/PokeApp';
 // import { TodoApp } from './useReducer/TodoApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
   <React.StrictMode>
     <MainApp />
   </React.StrictMode>
+  </BrowserRouter>
 )
