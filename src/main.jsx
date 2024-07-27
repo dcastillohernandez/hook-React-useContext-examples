8225d95361c9bb96d515a63a08b5a9a9b22dee96
dayronCastilloH <thecodedevcompany0@gmail.com>
2024-07-27 01:20:31 -0600

    build some pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css';
+import { MainApp } from './useContext/MainApp';
 
 // import { Padre } from './usando-memo/Padre';
 // import { PokeApp } from './pokemon-app/PokeApp';
-import { TodoApp } from './useReducer/TodoApp';
+// import { TodoApp } from './useReducer/TodoApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TodoApp />
+    <MainApp />
   </React.StrictMode>
 )
