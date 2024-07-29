de834756f5e4cbc7d913fade84e1acf61b641a61
dayronCastilloH <thecodedevcompany0@gmail.com>
2024-07-29 11:07:04 -0600

    Adding Routes in MainApp

--- src/useContext/MainApp.jsx
+++ src/useContext/MainApp.jsx
@@ -1,5 +1,7 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { HomePage } from './HomePage';
+import { AboutPage } from './AboutPage';
+import { LoginPage } from './LoginPage';
 
 export const MainApp = () => {
     return (
@@ -11,7 +13,13 @@ export const MainApp = () => {
             <Routes>
                 {/* Asegurarse de importar Route de react-router-dom */}
                 {/* El componente Route tiene 2 propiedades: path y element */}
-                <Route path='/' element={ <HomePage /> }/>
+                <Route path="/"     element={<HomePage />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="about" element={<AboutPage />} />
+
+                {/* El componente Navigate tiene la propiedad to */}
+                {/* La diferencia es que con el Navigate redirecciona al path que le indiquemos y no queda la url con el error */}
+                <Route path="/*" element={<Navigate to="/" />} />
             </Routes>
         </>
     )
