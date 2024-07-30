6380889574a71d1efcfec8d499856f81059c089a
dayronCastilloH <thecodedevcompany0@gmail.com>
2024-07-29 23:38:25 -0600

    Add NavLink for create active state in navbar

--- src/useContext/MainApp.jsx
+++ src/useContext/MainApp.jsx
@@ -1,12 +1,10 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { HomePage } from './HomePage';
-import { AboutPage } from './AboutPage';
-import { LoginPage } from './LoginPage';
+import { HomePage, AboutPage, LoginPage, Navbar } from './';
 
 export const MainApp = () => {
     return (
         <>
-            <h1>MainApp</h1>
+            <Navbar />
             <hr />
 
             {/* Asegurarse de importar Routes de react-router-dom */}
@@ -17,6 +15,7 @@ export const MainApp = () => {
                 <Route path="login" element={<LoginPage />} />
                 <Route path="about" element={<AboutPage />} />
 
+                {/* Cuando usamos el path * significa que atrapa cualquier path que no tengamos definidos en las rutas */}
                 {/* El componente Navigate tiene la propiedad to */}
                 {/* La diferencia es que con el Navigate redirecciona al path que le indiquemos y no queda la url con el error */}
                 <Route path="/*" element={<Navigate to="/" />} />
